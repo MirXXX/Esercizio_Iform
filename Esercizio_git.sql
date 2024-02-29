badb056acde40f55ca00be33545a22d342e8afcc
Camerada90 <156686320+Camerada90@users.noreply.github.com>
2024-02-29 12:23:09 +0100

    Modifica struttura tabelle

--- Esercizio_git.sql
+++ Esercizio_git.sql
@@ -1,29 +1,26 @@
 CREATE DATABASE esercizio_iform;
 USE esercizio_iform;
 
-CREATE TABLE users (
-    id_users INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-    Username VARCHAR(255),
-    nome VARCHAR(100),
-    cognome VARCHAR(100),
-    indirizzo VARCHAR(255),
-    email VARCHAR(255),
-    is_admin boolean,
-    active_ timestamp DEFAULT CURRENT_TIMESTAMP,
-    password_ VARCHAR(255)
-    );
-
-    CREATE TABLE esercizio_iform.regioni (
-id_regione integer NOT NULL AUTO_INCREMENT,
-nome varchar (255),
-primary key (id_regione)
+CREATE TABLE IF NOT EXISTS esercizio_iform.regioni (
+    id_regione integer NOT NULL AUTO_INCREMENT,
+    nome varchar (255),
+    primary key (id_regione)
 );
 
-
-CREATE TABLE citta {
+CREATE TABLE IF NOT EXISTS esercizio_iform.citta (
     id_citta integer NOT NULL AUTO_INCREMENT,
     nome varchar (255),
-    id_regione integer,
+    id_regione integer NOT NULL,
     primary key (id_citta)
-};
+);
 
+CREATE TABLE IF NOT EXISTS esercizio_iform.utenti (
+    id_utente integer NOT NULL AUTO_INCREMENT,
+    username varchar (255) NOT NULL,
+    password_ varchar (255) NOT NULL,
+    email varchar (255) NOT NULL,
+    active_ timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    is_admin boolean default 0,
+    id_citta integer,
+    primary key (id_utente)
+);
