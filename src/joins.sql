4015c60c6f476ca1005fe03622c1ec9cdadb1331
FabioAho <fabioarca.88@gmail.com>
2024-03-05 11:27:20 +0100

    add left right outer

--- src/joins.sql
+++ src/joins.sql
@@ -1,3 +1,19 @@
 SELECT id_utente, username, email, citta.nome
 from utenti 
 INNER JOIN citta ON utenti.id_citta=citta.id_citta;
+
+SELECT id_utente, username, email, citta.nome
+FROM utenti 
+LEFT JOIN citta ON utenti.id_citta=citta.id_citta;
+
+SELECT id_utente, username, email, citta.nome
+FROM utenti
+RIGHT JOIN citta ON utenti.id_citta=citta.id_citta;
+
+SELECT id_utente, username, email, citta.nome
+FROM utenti 
+LEFT JOIN citta ON utenti.id_citta=citta.id_citta
+UNION
+SELECT id_utente, username, email, citta.nome
+FROM utenti
+RIGHT JOIN citta ON utenti.id_citta=citta.id_citta;
